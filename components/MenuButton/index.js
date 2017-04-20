f4ac9e357eeb1064a58f00607487c79be8b52697
Grayson Hicks <graysonhicks@gmail.com>
2017-04-20 09:36:29 -0400

    button

---
+++ components/MenuButton/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    VrButton,
+} from 'react-vr';
+
+export default class Button extends Component {
+    constructor() {
+        super();
+        this.styles = StyleSheet.create({
+            button: {
+                margin: 0.05,
+                height: 0.4,
+                backgroundColor: 'red'
+            },
+            text: {
+                fontSize: 0.3,
+                textAlign: 'center',
+                color: 'white'
+            },
+        });
+    }
+    render() {
+        return (
+            <VrButton style = {this.styles.button} >
+                <Text style = {this.styles.text}> {
+                this.props.text
+                } < /Text>
+            </VrButton > );
+    }
+}
--- index.vr.js
+++ index.vr.js
@@ -8,25 +8,18 @@ import {
   View,
 } from 'react-vr';
 
+import Button from './components/MenuButton/index.js'
+
 export default class vrWorkplace extends React.Component {
   render() {
     return (
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
-        </Text>
+
+        <Pano source={asset('office.jpg')}/>
+        <Button text="Hello" />
+
+
+
       </View>
     );
   }
